0c2b1f05598e57570c0fd9efd01b7de7001bcacb
deekshith <deekshith@Mac-mini.local>
2023-01-27 03:17:30 +0530

    Learnt about express framework

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -15,6 +15,18 @@ app.post("/", (req, res) => {
   res.send("The sum is: " + (n1 + n2));
 });
 
+app.get("/bmiCalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", (req, res) => {
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+
+  var bmi = height / (weight * weight);
+  res.send("Your BMI is: " + bmi);
+});
+
 app.listen(3000, () => {
   console.log("Server is now running on port 3000....");
 });
