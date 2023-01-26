99cc754d571f77880cd8a9455e8eca781601f27d
deekshith <deekshith@Mac-mini.local>
2023-01-27 02:58:30 +0530

    learnt express

---
+++ calculator/calculator.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  var n1 = Number(req.body.num1);
+  var n2 = Number(req.body.num2);
+
+  res.send("The sum is: " + (n1 + n2));
+});
+
+app.listen(3000, () => {
+  console.log("Server is now running on port 3000....");
+});
